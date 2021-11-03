4fc9fdf4e599e0133bf946c9a825d8749b099ead
Rafael santos <rafael_16001@hotmail.com>
2021-11-03 12:33:48 -0300

    Projeto criado

---
+++ backend/src/main/java/com/projetoPI/SystemRod/SystemRodApplication.java
@@ -0,0 +1,13 @@
+package com.projetoPI.SystemRod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemRodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetoPI/SystemRod/SystemRodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoPI.SystemRod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
