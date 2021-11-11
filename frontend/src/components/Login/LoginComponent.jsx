63af23ec39b97ca8dcfc9af2ed81e82ba7fae76f
Rafael santos <rafael_16001@hotmail.com>
2021-11-11 09:13:02 -0300

    Atualização do login

--- frontend/src/components/Login/LoginComponent.jsx
+++ frontend/src/components/Login/LoginComponent.jsx
@@ -1,60 +1,63 @@
 import React, { Component } from 'react'
+import FormGroup from './FormGroup';
+import Passo from './Passo';
+
 
 class LoginComponent extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            login: '',
+            senha: ''
 
 
         }
     }
 
+    entrar = () => {
+        console.log('Login: ', this.state.login)
+        console.log('Senha: ', this.state.senha)
+    }
+
     render() {
         return (
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6-1">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 offset-md-3" >
+                        <div className="cent">
+                        <div className="bs-docs-section">
+                            <Passo title="Login">
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="bs-component">
+                                            <fieldset>
+                                                <FormGroup label="Login: *" htmlFor="exampleInputLogin" >
+                                                    <input type="login" className="form-control"
+                                                    value={this.state.login}
+                                                    onChange={e => this.setState({login: e.target.value})}
+                                                    id="exampleInputLogin" aria-describedby="loginHelp"
+                                                    placeholder="Digite o Login" />
+                                                </FormGroup>
+                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password" className="form-control"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({senha: e.target.value})}
+                                                    id="exampleInputPassword1" 
+                                                    placeholder="Password" />
+                                                </FormGroup>
+                                                <button onClick={this.entrar} className="btn btn-success">Entrar</button>
 
-                        <div class="bs-docs-section">
-
-                            <div class="card mb-3">
-                                <h3 class="card-header">Login</h3>
-                                <div class="card-body">
-
-
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="bs-component">
-                                                <form>
-                                                    <fieldset>
-                                                        <div class="form-group">
-                                                            <label for="exampleInputEmail1">Email: *</label>
-                                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o Email" />
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <label for="exampleInputPassword1">Senha: *</label>
-                                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                                                        </div>
-
-
-                                                        <button onclick="window.location.href='home.html'" type="button" class="btn btn-success">Entrar</button>
-                                                        
-
-                                                    </fieldset>
-                                                </form>
-                                            </div>
-
+                                            </fieldset>
                                         </div>
-
                                     </div>
                                 </div>
-                            </div>
 
-
+                            </Passo>
+                        </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         )
